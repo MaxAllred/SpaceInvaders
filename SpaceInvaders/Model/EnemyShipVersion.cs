4b1378daea49d038d6e8212679ab594959af341c
JaneraSmith <jsmit189@my.westga.edu>
2021-11-28 10:01:19 -0500

    Included an EnemyShipVersion enum to keep track of each enemy ship level.

---
+++ SpaceInvaders/Model/EnemyShipVersion.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceInvaders.Model
+{
+    /// <summary>
+    ///     This enum will keep track of each level of enemy ships.
+    /// </summary>
+    public enum EnemyShipVersion
+    {
+        LevelOne,
+        LevelTwo,
+        LevelThree,
+        LevelFour
+    }
+}
