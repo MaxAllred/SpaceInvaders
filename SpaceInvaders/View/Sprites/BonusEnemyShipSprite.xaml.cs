95700bcedf0e39f3ab98d32504b18970b6ace705
JaneraSmith <jsmit189@my.westga.edu>
2021-12-02 09:02:57 -0500

    Created a Bonus Enemy Sprite for the game.

---
+++ SpaceInvaders/View/Sprites/BonusEnemyShipSprite.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites
+{
+    /// <summary>
+    ///     Manages a Bonus enemy ship.
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    public sealed partial class BonusEnemyShipSprite
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BonusEnemyShipSprite"/> class.
+        /// </summary>
+        public BonusEnemyShipSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
