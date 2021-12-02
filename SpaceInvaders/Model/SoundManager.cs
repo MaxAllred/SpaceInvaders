ed1f463d21dba71c7ac6ecf3bd979be7bfeb1415
JaneraSmith <jsmit189@my.westga.edu>
2021-12-02 08:32:09 -0500

    Added an extra sound for when the player wins the game.

--- SpaceInvaders/Model/SoundManager.cs
+++ SpaceInvaders/Model/SoundManager.cs
@@ -6,35 +6,65 @@ using System.Threading.Tasks;
 
 namespace SpaceInvaders.Model
 {
-    class SoundManager
+    /// <summary>
+    ///     Manages a Sound manager for each sound to be played for playership, enemies, bullets, and collisions.
+    /// </summary>
+    public class SoundManager
     {
         private SoundPlayer sp;
 
+        /// <summary>
+        ///     Plays the player shot sound.
+        /// </summary>
         public void playerShot()
         {
             sp = new SoundPlayer();
             sp.Play(@"Assets/Audio/Flash-laser-03.wav", "playerShot");
         }
+
+        /// <summary>
+        ///  Plays the enemy shot sound.
+        /// </summary>
         public void enemyShot()
         {
             sp = new SoundPlayer();
             sp.Play(@"Assets/Audio/Flash-laser-09.wav", "enemyShot");
         }
+
+        /// <summary>
+        ///     Plays the player bullet hit sound.
+        /// </summary>
         public void playerBulletHit()
         {
             sp = new SoundPlayer();
             sp.Play(@"Assets/Audio/Boink-single-high-01.wav", "playerShotHit");
         }
+
+        /// <summary>
+        /// Plays the enemy bullet hit sound.
+        /// </summary>
         public void enemyBulletHit()
         {
             sp = new SoundPlayer();
             sp.Play(@"Assets/Audio/Rubber-snap-01.wav", "enemyShotHit");
         }
 
+        /// <summary>
+        ///     Plays the game over sound.
+        /// </summary>
         public void gameOver()
         {
             sp = new SoundPlayer();
             sp.Play(@"Assets/Audio/gameover.wav", "gameOver");
+        }
+
+        /// <summary>
+        ///  Plays the you win sound.
+        /// </summary>
+        public void youWin()
+        {
+            sp = new SoundPlayer();
+            sp.Play(@"Assets/Audio/mixkit-video-game-win-2016.wav", "youWin");
         }
     }
 }
