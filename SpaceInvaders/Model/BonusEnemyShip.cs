603b2bafd65ed10c92319700b0b3beb6ee8e92ca
JaneraSmith <jsmit189@my.westga.edu>
2021-12-02 09:27:48 -0500

    Created Bonus Enemy Ship class in Model folder.

---
+++ SpaceInvaders/Model/BonusEnemyShip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model
+{
+    /// <summary>
+    ///     Manages the bonus enemy ship.
+    /// </summary>
+    public class BonusEnemyShip : EnemyShip
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BonusEnemyShip"/> class.
+        /// </summary>
+        public BonusEnemyShip()
+        {
+            Sprite = new BonusEnemyShipSprite();
+            this.PointValue = 50;
+            this.CanShoot = true;
+
+            SetSpeed(10, 1);
+        }
+    }
+}
