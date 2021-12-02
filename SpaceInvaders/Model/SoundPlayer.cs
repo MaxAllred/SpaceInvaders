202f4c55426eae1b095ca4e3f282c8ed752139c1
JaneraSmith <jsmit189@my.westga.edu>
2021-12-02 08:41:13 -0500

    Added documentation inside SoundPlayer class.

--- SpaceInvaders/Model/SoundPlayer.cs
+++ SpaceInvaders/Model/SoundPlayer.cs
@@ -7,11 +7,21 @@ using System.Threading.Tasks;
 
 namespace SpaceInvaders.Model
 {
-    class SoundPlayer
+    /// <summary>
+    ///     This class will play a sound for the game.
+    /// </summary>
+    public class SoundPlayer
     {
         [DllImport("winmm.dll")]
         static extern Int32 mciSendString(string command, StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
 
+        /// <summary>
+        ///     Plays the specified path.
+        ///     Precondition: path != null && name != null
+        ///     Postcondition: Play a sound from the path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="name">The name.</param>
         public void Play(string path, string name)
         {
 
