a3ff20b938e29e02e7facd1f38b3d2da30625ac2
JaneraSmith <jsmit189@my.westga.edu>
2021-12-04 12:44:38 -0500

    Added documentations in EnemyFactory class.

--- SpaceInvaders/Model/EnemyFactory.cs
+++ SpaceInvaders/Model/EnemyFactory.cs
@@ -1,9 +1,15 @@
 ﻿namespace SpaceInvaders.Model
 {
+    /// <summary>
+    ///     Manages an Enemy Factory for the enemy ships.
+    /// </summary>
     public abstract class EnemyFactory
     {
-        #region Methods
-
+        #region Methods        
+        /// <summary>
+        ///     Gets the enemy ship.
+        /// </summary>
+        /// <returns> An enemy ship.</returns>
         public abstract EnemyShip GetEnemyShip();
 
         #endregion
