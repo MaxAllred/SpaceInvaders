e50fda461c6644b7065f8dd0f8f2c57420760cab
JaneraSmith <jsmit189@my.westga.edu>
2021-12-04 12:15:51 -0500

    Did ReSharper on BonusEnemyShip

--- SpaceInvaders/Model/BonusEnemyShip.cs
+++ SpaceInvaders/Model/BonusEnemyShip.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SpaceInvaders.View.Sprites;
+﻿using SpaceInvaders.View.Sprites;
 
 namespace SpaceInvaders.Model
 {
@@ -12,16 +7,20 @@ namespace SpaceInvaders.Model
     /// </summary>
     public class BonusEnemyShip : EnemyShip
     {
+        #region Constructors
+
         /// <summary>
-        ///     Initializes a new instance of the <see cref="BonusEnemyShip"/> class.
+        ///     Initializes a new instance of the <see cref="BonusEnemyShip" /> class.
         /// </summary>
         public BonusEnemyShip()
         {
             Sprite = new BonusEnemyShipSprite();
-            this.PointValue = 50;
-            this.CanShoot = true;
+            PointValue = 50;
+            CanShoot = true;
 
             SetSpeed(12, 1);
         }
+
+        #endregion
     }
 }
